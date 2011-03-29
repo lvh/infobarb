139f466e2bc5b3b3888ed98069f6ecf1f5668dea
Laurens Van Houtven <_@lvh.cc>
2011-03-29 18:56:54 +0200

    Major changes to how the client and the fancy interface work -- everything is broken \o/

--- infobarb/irc.py
+++ infobarb/irc.py
@@ -1,40 +1,77 @@
 """
 Low level abstraction layer over IRC.
 """
+import inspect
+
 import panglery
 
 from twisted.words.protocols import irc
 
 
+_supportedEvents = {
+    "userJoined": {
+        "callback": "userJoined"
+        }
+    }
+
+
+
+class InfobarbClient(irc.IRCClient):
+    """
+    The IRC client for an infobarb.
+    """
+    nickname = "infobarb"
+
+    def __init__(self, pangler):
+        self.p = pangler
+
+
+
+def _buildCallback(eventName, argNames):
+    """
+    Builds a callback method for IRCClient.
+
+    Triggers the specified event with all arguments passed to the callback.
+    """
+    def callback(self, *args):
+        assert len(argNames) == len(args) # inspection sanity check
+        kwargs = dict(zip(argNames, args))
+        self.p.trigger(event=eventName, **kwargs)
+
+    return callback
+
+
+for eventName in _supportedEvents:
+    eventInfo = _supportedEvents[eventName]
+    callbackName = eventInfo["callback"]
+
+    original = getattr(InfobarbClient, callbackName)
+    eventInfo["args"] = argNames = inspect.getargspec(original).args[1:]
+    
+    callback = _buildCallback(eventName, argNames)
+    setattr(InfobarbClient, callbackName, callback)
+
+
+
+_shortcuts = {
+    # "privateMessageReceived": "onPrivateMessage",
+    # "channelMessageReceived": "onChannelMessage",
+    # "privateNoticeReceived": "onPrivateNotice",
+    # "channelNoticeReceived": "onChannelNotice",
+    "userJoined": "onUserJoin"
+    }
+
+
+
 class FancyInfobarbPangler(object):
     """
     Some infobarb-specific event hook shortcuts.
+
+    This is just syntactic sugar. No fancy behavior here!
     """
     def __init__(self, boundPangler):
         self.p = boundPangler
 
-
-    _SHORTCUTS = {
-        "onPrivateMessage": {
-            "event": "privateMessageReceived",
-            "needs": ("user", "message")},
-
-        "onChannelMessage": {
-            "event": "channelMessageReceived",
-            "needs": ("user", "channel", "message")},
-
-        "onPrivateNotice": {
-            "event": "privateNoticeReceived",
-            "needs": ("user", "message")},
-
-        "onChannelNotice": {
-            "event": "channelNoticeReceived",
-            "needs": ("user", "channel", "message")},
-
-        "onUserJoin": {
-            "event": "userJoined",
-            "needs": ("user", "channel")}
-        }
 
 
 def _buildShortcut(defaultKwargs):
@@ -48,43 +85,27 @@ def _buildShortcut(defaultKwargs):
     return shortcut
 
 
-for name, defaultKwargs in FancyInfobarbPangler._SHORTCUTS.iteritems():
-    setattr(FancyInfobarbPangler, name, _buildShortcut(defaultKwargs))
+
+for event, shortcutName in _shortcuts.iteritems():
+    kwargs = {"event": event, "needs": _supportedEvents[event]["args"]}
+    setattr(FancyInfobarbPangler, shortcutName, _buildShortcut(kwargs))
 
 
-class InfobarbClient(irc.IRCClient):
-    """
-    The IRC client for an infobarb.
-    """
-    nickname = "infobarb"
 
-    def __init__(self, pangler):
-        self.p = pangler
+# TODO
+def privmsgReceived(self, p, user, channel, message):
+    if channel == self.nickname:
+        event = "privateMessageReceived"
+    else:
+        event = "channelMessageReceived"
 
-
-    def privmsg(self, user, channel, message):
-        if channel == self.nickname:
-            event = "privateMessageReceived"
-        else:
-            event = "channelMessageReceived"
-
-        self.p.trigger(event=event,
-                       user=user,
-                       channel=channel,
-                       message=message)
+    p.trigger(event=event, user=user, channel=channel, message=message)
 
 
-    def noticed(self, user, channel, message):
-        if channel == self.nickname:
-            event = "privateNoticeReceived"
-        else:
-            event = "channelNoticeReceived"
+def noticeReceived(self, p, user, channel, message):
+    if channel == self.nickname:
+        event = "privateNoticeReceived"
+    else:
+        event = "channelNoticeReceived"
 
-        self.p.trigger(event=event,
-                       user=user,
-                       channel=channel,
-                       message=message)
-
-
-    def userJoined(self, user, channel):
-        self.p.trigger(event="userJoined", user=user, channel=channel)
+    p.trigger(event=event, user=user, channel=channel, message=message)
